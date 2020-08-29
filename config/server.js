95c7e94b4b7d36c9a515d3f68b3af9a9313906ca
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2020-08-29 18:16:56 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2dff7551645e8e95502d8f6a7a4332a1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '49813c2b-7fa1-463b-9ca0-5a9be8f14664'
+};
