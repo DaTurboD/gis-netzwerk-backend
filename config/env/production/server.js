bc5a4070389e283e4395384abd951bdba2444412
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2020-08-30 09:21:22 +0200

    environments

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '45.136.29.31'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2dff7551645e8e95502d8f6a7a4332a1'),
     },
   },
+  proxy: {
+    enabled: true,
+    ssl: true,
+    host: "api.gis-netzwerk.com",
+    port: 1337
+  }
 });
