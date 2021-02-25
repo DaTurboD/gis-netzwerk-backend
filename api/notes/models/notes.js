15cbaa7ea02dbcbb6345ae6091f1e69310864d87
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-02-25 09:39:47 +0100

    test

--- api/notes/models/notes.js
+++ api/notes/models/notes.js
@@ -1,8 +1,21 @@
 'use strict';
-
+const fetch = require("node-fetch");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+const sendWebmention = (data) => {
+    async function sendData() {
+      console.log("hi")
+    }
+    data.publishOnTwitter ? sendData() : null;
+}
 
-module.exports = {};
+module.exports = {
+
+    lifecycles: {
+        afterCreate(result) {
+            sendWebmention(result)
+        },
+    }
+};
