42bcd839cb62c0aafa8645d64edc54e272a680bd
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-03-19 17:26:04 +0100

    Update notes.js

--- api/notes/models/notes.js
+++ api/notes/models/notes.js
@@ -6,26 +6,12 @@ const axios = require('axios');
  * to customize this model
  */
 
-async function getWeatherData(data) {
-
-    const currentLocation = await strapi.config.functions.getLocation();
-    const cords = data.lat == null && data.lon == null ? [currentLocation.lat, currentLocation.lon] : [data.lat, data.lon]
-    const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${cords[0]}&lon=${cords[1]}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`)
-    return {
-        cords: cords,
-        temp: weather.data.main.temp,
-        weather_main: weather.data.weather[0].main,
-        weather_description: weather.data.weather[0].description,
-        weather_icon: weather.data.weather[0].icon
-    }
-}
-
 module.exports = {
     lifecycles: {
         async beforeCreate(data) {
 
             // set weather data
-            const weatherData = await getWeatherData(data)
+            const weatherData = await strapi.config.functions.getWeather(data)
             console.log(weatherData)
             data.temperature = weatherData.temp
             data.weather_main = weatherData.weather_main
