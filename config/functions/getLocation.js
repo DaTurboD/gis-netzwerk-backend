dd4e1915115f312de47564f8d258b2e9188566be
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-03-19 17:17:57 +0100

    Update getLocation.js

--- config/functions/getLocation.js
+++ config/functions/getLocation.js
@@ -1,11 +1,10 @@
 const axios = require('axios');
 
 module.exports = async () => {
-  async function getCurrentLocation() {
-    const locations = await axios.get(`https://api.mxd.codes/locations?_sort=id:desc`)
+  const locations = await axios.get(`https://api.mxd.codes/locations?_sort=id:desc`)
 
-    return {
-        lat: locations.data[0].lat, 
-        lon: locations.data[0].lon
-    }
+  return {
+    lat: locations.data[0].lat, 
+    lon: locations.data[0].lon
+  }
 }
