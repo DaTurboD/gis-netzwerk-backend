ea1c158b05005a776203896de6dac7f77bd17b31
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-03-29 20:04:34 +0200

    cron

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -3,7 +3,7 @@
 module.exports = {
 // Add your own logic here (e.g. send a queue of email, create a database backup, etc.).
 
-    '* * 21 * * *': () => {
+    '0 0 21 * * *': () => {
       strapi.config.functions.getGarminConnectActivities();
 
     },
